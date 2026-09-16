1b8d6919a045fe791150a6c4626c86c24c99fac5
402-30-jarupishaya <54435@yupparaj.ac.th>
2026-09-16 15:12:50 +0700

    Update furniture_game.py

--- furniture_game.py
+++ furniture_game.py
@@ -9,6 +9,12 @@ if "ans1_val" not in st.session_state:
     st.session_state.ans1_val = ""
 if "ans2_val" not in st.session_state:
     st.session_state.ans2_val = ""
+    if "ans3_val" not in st.session_state:
+    st.session_state.ans3_val = ""
+if "ans4_val" not in st.session_state:
+    st.session_state.ans4_val = ""
+    if "ans5_val" not in st.session_state:
+    st.session_state.ans5_val = ""
 if "game_started" not in st.session_state:
     st.session_state.game_started = False
 
@@ -17,6 +23,9 @@ if "game_started" not in st.session_state:
 def reset_game():
     st.session_state.ans1_val = ""  # เคลียร์ค่าช่องข้อ 1
     st.session_state.ans2_val = ""  # เคลียร์ค่าช่องข้อ 2
+     st.session_state.ans3_val = ""  # เคลียร์ค่าช่องข้อ 3
+    st.session_state.ans4_val = ""  # เคลียร์ค่าช่องข้อ 4 
+    st.session_state.ans5_val = ""  # เคลียร์ค่าช่องข้อ 5
     st.session_state.start = time.time()  # เริ่มเวลาใหม่
     st.session_state.is_ended = False  # ปิด Dialog
     st.session_state.game_started = True  # ทำการเริ่มเกม
@@ -32,6 +41,10 @@ def show_result_dialog(ans1, ans2):
 
     u_ans1 = ans1.strip().lower()
     u_ans2 = ans2.strip().lower()
+  u_ans3 = ans3.strip().lower()
+    u_ans4 = ans4.strip().lower()
+  u_ans5 = ans5.strip().lower()
+    
 
     # ตรวจข้อ 1
     if u_ans1 == "bed":
@@ -48,13 +61,38 @@ def show_result_dialog(ans1, ans2):
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
     # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มตรวจข้อ 3, 4, 5 ตรงนี้
+   # ตรวจข้อ 3
+    if u_ans3 == " table":
+        st.success("✅ ข้อ 3: ถูกต้อง")
+        score += 1
+    else:
+        st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
+
+    # ตรวจข้อ 4
+    if u_ans4 == "sofa":
+        st.success("✅ ข้อ 4: ถูกต้อง")
+        score += 1
+    else:
+        st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
+     
+    # ตรวจข้อ 5
+    if u_ans5 == "mirror":
+        st.success("✅ ข้อ 5: ถูกต้อง")
+        score += 1
+    else:
+        st.error(f"❌ ข้อ 5: ยังไม่ถูกต้อง (คุณตอบ '{u_ans5}')")
 
     st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
 
-    if score == 2:
-        st.success("🎉 You win!")
+   if score == 5:
+        st.success("😍 โหดขั้นเทพระดับมาเฟีย")
+     if score == 4:
+      st.success("🎉 คนธรรมด๊าาา ธรรมดาา")
+     #if score == 0:
+     # st.success("🐔 กระจอกระดับขี้ไก่")
+
     else:
-        st.error("💀 You lose!")
+        st.error("🐔 กระจอกระดับขี้ไก่")
 
 
 # ----------------------------------------------------
@@ -78,7 +116,7 @@ st.divider()
 
 # 3. ช่องรับคำตอบ
 ans1 = st.text_input(
-    "ข้อ 1: `b _ d`.It supports the mattress and your body while sleeping.😴 ",
+    "ข้อ 1: `b _ d`.It supports the mattress and your body while sleeping.😴",
     key="ans1_val",
 )
 ans2 = st.text_input(
@@ -86,8 +124,19 @@ ans2 = st.text_input(
     key="ans2_val",
 )
 
-# ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มข้อ 3, 4 ตรงนี้
-
+# ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มข้อ 3, 4, 5 ตรงนี้
+ans3 = st.text_input(
+    "ข้อ 3: `_ a _ _ e`.Have legs but cannot walk. You can put your food or books on me.📚",
+    key="ans3_val",
+)
+ans4 = st.text_input(
+    "ข้อ 4:  `s _ _ a`.It features soft cushions and armrests, designed for multiple people to sit on.🛋",
+    key="ans4_val",
+)
+ans5 = st.text_input(
+    "ข้อ 5: `_ i r r _ r `.A glass surface that reflects your image when you look into it.🚽 ",
+    key="ans5_val",
+)
 
 # 4. ปุ่มส่งคำตอบ และระบบนับถอยหลัง Real-time
 if st.session_state.get("game_started", False) and not st.session_state.get(
@@ -103,4 +152,4 @@ if st.session_state.get("game_started", False) and not st.session_state.get(
 
 # 5. แสดง Dialog ผลลัพธ์
 if st.session_state.get("is_ended", False):
-    show_result_dialog(ans1, ans2)
+    show_result_dialog(ans1, ans2, ans3, ans4, ans5)
